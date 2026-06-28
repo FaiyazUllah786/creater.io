a0676171b2d8dd0d1ec9cc6cda637988e65b7ba5
Faiyaz Ullah <faiyazullah54@gmail.com>
2026-06-28 15:04:32 +0530

    fix: passport js usage fix

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,9 +4,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { accessTokenCookieOptions, refreshTokenCookieOptions } from "../utils/cookieOptions.js";
-import configurePassport from "../passport/auth.passport.js";
-
-const passport = configurePassport();
+import passport from "passport";
 
 export const generateAccessRefreshToken = async (userId) => {
   try {
